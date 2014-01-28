a431fa0b94778485f47c6446abea8e61a6cac9b1
rtiago42 <User@TRodriguess-Mac.local>
2014-01-28 16:10:24 +0000

    Added OBSQuery

--- OpenBaaS/OpenBaaS/Utilities/OBSQuery.h
+++ OpenBaaS/OpenBaaS/Utilities/OBSQuery.h
@@ -10,3 +10,17 @@
 
 extern NSString *const OBSQueryParamCollectionPage;
 extern NSString *const OBSQueryParamCollectionPageSize;
+extern NSString *const OBSQueryParamCollectionDataQuery;
+
+@interface OBSQuery : NSObject
+
++ (NSDictionary *)operationNotOperation:(NSDictionary *)operation;
++ (NSDictionary *)operationAndOfLeftOperation:(NSDictionary *)lOperation rightOperaton:(NSDictionary *)rOperation;
++ (NSDictionary *)operationOrOfLeftOperation:(NSDictionary *)lOperation rightOperaton:(NSDictionary *)rOperation;
+
++ (NSDictionary *)operationStringAtPath:(NSString *)path containsString:(NSString *)string;
++ (NSDictionary *)operationValueAtPath:(NSString *)path isEqualTo:(id)value;
++ (NSDictionary *)operationValueAtPath:(NSString *)path isGreaterThen:(id)value;
++ (NSDictionary *)operationValueAtPath:(NSString *)path isLesserThen:(id)value;
+
+@end
--- OpenBaaS/OpenBaaS/Utilities/OBSQuery.m
+++ OpenBaaS/OpenBaaS/Utilities/OBSQuery.m
@@ -10,3 +10,57 @@
 
 NSString *const OBSQueryParamCollectionPage = @"pageNumber";
 NSString *const OBSQueryParamCollectionPageSize = @"pageSize";
+NSString *const OBSQueryParamCollectionDataQuery = @"query";
+
+static NSString *const _kQueryOperationKey = @"oper";
+static NSString *const _kQueryLeftOperationKey = @"op1";
+static NSString *const _kQueryRightOperationKey = @"op2";
+static NSString *const _kQueryPath = @"attribute";
+static NSString *const _kQueryValue = @"value";
+
+@implementation OBSQuery
+
++ (NSDictionary *)operationNotOperation:(NSDictionary *)operation
+{
+    return @{_kQueryOperationKey: @"not",
+             _kQueryLeftOperationKey: operation};
+}
++ (NSDictionary *)operationAndOfLeftOperation:(NSDictionary *)lOperation rightOperaton:(NSDictionary *)rOperation
+{
+    return @{_kQueryOperationKey: @"and",
+             _kQueryLeftOperationKey: lOperation,
+             _kQueryRightOperationKey: rOperation};
+}
++ (NSDictionary *)operationOrOfLeftOperation:(NSDictionary *)lOperation rightOperaton:(NSDictionary *)rOperation
+{
+    return @{_kQueryOperationKey: @"or",
+             _kQueryLeftOperationKey: lOperation,
+             _kQueryRightOperationKey: rOperation};
+}
+
++ (NSDictionary *)operationStringAtPath:(NSString *)path containsString:(NSString *)string
+{
+    return @{_kQueryOperationKey: @"contains",
+             _kQueryPath: path,
+             _kQueryValue: string};
+}
++ (NSDictionary *)operationValueAtPath:(NSString *)path isEqualTo:(id)value
+{
+    return @{_kQueryOperationKey: @"equals",
+             _kQueryPath: path,
+             _kQueryValue: value};
+}
++ (NSDictionary *)operationValueAtPath:(NSString *)path isGreaterThen:(id)value
+{
+    return @{_kQueryOperationKey: @"greater",
+             _kQueryPath: path,
+             _kQueryValue: value};
+}
++ (NSDictionary *)operationValueAtPath:(NSString *)path isLesserThen:(id)value
+{
+    return @{_kQueryOperationKey: @"lesser",
+             _kQueryPath: path,
+             _kQueryValue: value};
+}
+
+@end
