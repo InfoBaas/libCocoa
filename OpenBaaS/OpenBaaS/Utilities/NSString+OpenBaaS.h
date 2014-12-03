ead1973294c80695e30fb9516074129e078b6ed7
Márcio Lopes <marcio.lopes@infosistema.com>
2014-12-03 18:05:32 +0000

    Changed Information
    Change licences

--- OpenBaaS/OpenBaaS/Utilities/NSString+OpenBaaS.h
+++ OpenBaaS/OpenBaaS/Utilities/NSString+OpenBaaS.h
@@ -2,9 +2,25 @@
 //  NSString+OpenBaaS.h
 //  OpenBaaS
 //
-//  Created by Tiago Rodrigues on 06/12/2013.
-//  Copyright (c) 2013 Infosistema. All rights reserved.
-//
+/*****************************************************************************************
+ Infosistema - Lib-Cocoa
+ Copyright(C) 2002-2014 Infosistema, S.A.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
+ You should have received a copy of the GNU Affero General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ www.infosistema.com
+ info@openbaas.com
+ Av. José Gomes Ferreira, 11 3rd floor, s.34
+ Miraflores
+ 1495-139 Algés Portugal
+ ****************************************************************************************/
 
 #import <Foundation/Foundation.h>
 
